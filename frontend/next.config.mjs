19110a3d9fd83967a9ecc52536d8b0fc65a867fc
Ahmed Kibria <gkibria121@gmail.com>
2026-01-21 00:29:49 +0600

    feat: update Next.js configuration to include additional image domains and streamline tsconfig formatting

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,7 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["res.cloudinary.com", "localhost"],
+    domains: [
+      "res.cloudinary.com",
+      "localhost",
+      process.env.DOMAIN,
+      "grand-sampan-resort.unitechholdingsltd.com",
+    ],
   },
 };
 export default nextConfig;
